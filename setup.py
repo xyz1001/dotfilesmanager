3638bde3ec1b83d584d699e497411071cc438811
xyz1001 <zgzf1001@gmail.com>
2021-04-09 14:18:24 +0800

    更新版本号，修改dotfiles_root

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dotfilesmanager',
-    version="1.0.4",
+    version="1.0.5",
     description="dotfiles管理工具",
     long_description="""dotfile管理工具，支持多平台""",
     keywords='python dotfiles',
